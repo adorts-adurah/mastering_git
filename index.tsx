433bcc29b2e578fcd41fc77c06f6de5ca7db5131
Adorts-Adurah <mail.durallite@gmail.com>
2025-10-20 01:42:31 -0700

    git commiting new now for testing purspoase

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="justify-center alingn-center">
+      <Text className="text-primary">Hselo</Text>
+    </View>
+  );
+}
+ 
